55c3726faca1a1060c315e86a0386ecf8e82f2e1
Lakshay Kansal <laksh.kansal@gmail.com>
2022-11-22 22:52:42 -0500

    counter works

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState, useMemo, useCallback } from 'react';
+import CountUp from 'react-countup';
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    useParams,
+    useSearchParams,
+    useLocation,
+} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
+}
+
+function Home() {
+
+    const [mode, setMode] = useState('write');
+    const [count, setCount] = useState('read');
+
+    useEffect(() => {
+        if(mode == 'write'){
+            fetch('https://0tc8svpio2.execute-api.us-east-1.amazonaws.com/default/operation-hcps',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({type: mode}),
+            }).then((response) => response.json()).then((response) => setCount(response['count']))
+            setMode('read');
+        }else{
+        fetch('https://0tc8svpio2.execute-api.us-east-1.amazonaws.com/default/operation-hcps',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({type: mode}),
+        }).then((response) => response.json()).then((response) => setCount(response['count']))
+        }
+    })
+
+    return (
+        <div className="h-all">
+            <div>visits</div>
+            <div className="h-visits">
+                <CountUp end={count} duration={5} useEasing={true} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
